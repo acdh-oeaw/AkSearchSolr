541287fb184c986b40db3b4fbcaab8697601a05f
Massimiliano Carloni <79900686+carlonim@users.noreply.github.com>
2022-04-21 18:52:30 +0200

    Add custom method to index MARC 700 properly
    Custom methods getRepeated and getSubfieldAtLeastOnce are now implemented to index properly MARC 700 fields with missing or multiple $4 subfields

--- java_helpers/Oeaw.java
+++ java_helpers/Oeaw.java
@@ -37,8 +37,42 @@ public class Oeaw {
                 value = value.trim();
                 // handle repetition
                 int count = field.getSubfields(countSubfield.charAt(0)).size();
+		if (count == 0) {
+                    count = 1;
+                }
                 for (int i = 0; i < count; i++) {
                     result.add(value);
+                }
+            }
+        }
+        return result;
+    }
+
+    public Collection<String> getSubfieldAtLeastOnce(Record record, String description, String defaultValue) {
+        List<String> result = new LinkedList<String>();
+        DataField field;
+        for (String desc : description.split(":")) {
+            String[] parts = desc.split("[|]");
+            String fieldNo = parts[0];
+            char ind1 = parts[1].charAt(0);
+            char ind2 = parts[1].charAt(1);
+            char subfieldCode = parts[2].charAt(0);
+            for (VariableField varField : record.getVariableFields(parts[0])) {
+                field = (DataField) varField;
+                // filter based on indicators
+                if (!(ind1 == '_' || ind1 == '#' && field.getIndicator1() == ' ' || ind1 == field.getIndicator1())) {
+                    continue;
+                }
+                if (!(ind2 == '_' || ind2 == '#' && field.getIndicator2() == ' ' || ind2 == field.getIndicator2())) {
+                    continue;
+                }
+		boolean hit = false;
+                for (Subfield subfield : field.getSubfields(subfieldCode)) {
+                    result.add(subfield.getData().trim());
+		    hit = true;
+		}
+                if (!hit) {
+                    result.add(defaultValue);
                 }
             }
         }
