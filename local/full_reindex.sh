ca78d675e4a13f70a61fbf6c15374b8c0f47186c
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2022-05-24 16:04:05 +0200

    clear core by script

---
+++ local/full_reindex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Resetting Core"
+curl --request POST -sL \
+     --url 'http://localhost:8983/api/cores/biblio/config'\
+     -d '{"set-property": [{"requestDispatcher.requestParsers.enableRemoteStreaming": true},{"requestDispatcher.requestParsers.enableStreamBody": true}]}'\
+     -H "Content-Type: application/json"
+
+curl --request GET -sL \
+     --url 'http://localhost:8983/solr/biblio/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
+
+
+
+
+
