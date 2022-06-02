517195a8fc6ff03b27d685e2abc661d65ffe0c93
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-02 10:34:03 +0200

    aksearch.sh: honor SOLR_HOME

--- aksearch.sh
+++ aksearch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+SOLR_HOME=${SOLR_HOME:=/opt/solr/server/solr}
 if [ ! -d /opt/solr/server/solr/mycores/biblio ]; then
     echo "AkSearch cores missing - initializing"
-    cp -R /opt/aksearch/solr/vufind/* /opt/solr/server/solr/mycores/
-    mv /opt/solr/server/solr/mycores/solr.xml /opt/solr/server/solr/solr.xml
+    cp -R /opt/aksearch/solr/vufind/* "$SOLR_HOME/mycores/"
+    mv "$SOLR_HOME/mycores/solr.xml" "$SOLR_HOME/solr.xml"
 fi
 
