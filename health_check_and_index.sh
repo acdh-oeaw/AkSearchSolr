e0dbca62714135699025ea30ff6c57e0aabe3ad0
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-03 13:16:01 +0200

    health_check_and_index.sh: start indexing only if new files are present

--- health_check_and_index.sh
+++ health_check_and_index.sh
@@ -4,12 +4,12 @@ RET=$?
 
 # minutes since last activity in the directory to start indexing
 DELAY=$1
-DELAY=${DELAY:=60}
+DELAY=${DELAY:=10}
 
-if [ "`find /opt/harvest/alma -maxdepth 0 -mmin -$DELAY`" == "" ] ; then
+if [ "`find /opt/harvest/alma -maxdepth 0 -mmin -$DELAY`" == "" ] && [ "`find /opt/harvest/alma -maxdepth 1 -name *xml | wc -l`" != "0" ] ; then
     nohup /opt/aksearch/harvest/batch-import-marc-single.sh -d /opt/harvest/alma > /opt/harvest/alma.log 2>&1
 fi
-if [ "`find /opt/harvest/degruyter -maxdepth 0 -mmin -$DELAY`" == "" ] ; then
+if [ "`find /opt/harvest/degruyter -maxdepth 0 -mmin -$DELAY`" == "" ] && [ "`find /opt/harvest/degruyter -maxdepth 1 -name *xml | wc -l`" != "0" ] ; then
     #nohup /opt/aksearch/harvest/batch-import-marc-single.sh -d /opt/harvest/degruyter > /opt/harvest/degruyter.log 2>&1
     echo "degruyter import not defined yet"
 fi
