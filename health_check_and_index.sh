850804db0d7310b871264417a7ef9dea5ccbf528
Mateusz Żółtak <zozlak@zozlak.org>
2022-06-03 08:34:20 +0200

    health_check_and_index.sh: fix degruyter if and allow to enforce the delay with a parameter

--- health_check_and_index.sh
+++ health_check_and_index.sh
@@ -3,13 +3,15 @@ curl http://127.0.0.1:8983/solr/ > /dev/null 2>&1
 RET=$?
 
 # minutes since last activity in the directory to start indexing
-DELAY=60
+DELAY=$1
+DELAY=${DELAY:=60}
 
 if [ "`find /opt/harvest/alma -maxdepth 0 -mmin -$DELAY`" == "" ] ; then
     nohup /opt/aksearch/harvest/batch-import-marc-single.sh -d /opt/harvest/alma > /opt/harvest/alma.log 2>&1
 fi
 if [ "`find /opt/harvest/degruyter -maxdepth 0 -mmin -$DELAY`" == "" ] ; then
     #nohup /opt/aksearch/harvest/batch-import-marc-single.sh -d /opt/harvest/degruyter > /opt/harvest/degruyter.log 2>&1
+    echo "degruyter import not defined yet"
 fi
 
 exit $RET
