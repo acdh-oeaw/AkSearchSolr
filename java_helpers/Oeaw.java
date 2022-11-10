10c4112121a7a6713dad89a8064d9b2b55710359
Mateusz Żółtak <zozlak@zozlak.org>
2022-11-10 17:56:44 +0100

    java_helpers/Oeaw.java: skipTagsAndSort(skipContent) helper method added
    The method:
    
    * removes the DACH-MARC "skip" from sorting tags (<< and >>)
    * optionally removes the content mark with them (when called with the
      "true" parameter)
    * returns output sorted according to the values with the <<content>>
      removed
    
    Can be used in the custom .properties file by attaching the
    `custom_map(Oeaw skipTagsAndSort(false)` or  `custom_map(Oeaw skipTagsAndSort(true)`
    as the last transformation, e.g.:
      fieldName = custom, getAuthorsFilteredByRelator(110abg:111abcg:710abg:711abg,110:111:710:711,firstAuthorRoles|secondAuthorRoles), custom_map(Oeaw skipTagsAndSort(false))
    or
      fieldName = 245ab, first, custom_map(Oeaw skipTagsAndSort(true))

--- java_helpers/Oeaw.java
+++ java_helpers/Oeaw.java
@@ -1,6 +1,7 @@
 import java.util.Collection;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.TreeMap;
 import org.marc4j.marc.Record;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.Subfield;
@@ -101,17 +102,28 @@ public class Oeaw {
                 if (!(ind2 == '_' || ind2 == '#' && field.getIndicator2() == ' ' || ind2 == field.getIndicator2())) {
                     continue;
                 }
-		boolean hit = false;
+                boolean hit = false;
                 for (Subfield subfield : field.getSubfields(subfieldCode)) {
                     result.add(subfield.getData().trim());
-		    hit = true;
-		}
+                    hit = true;
+                }
                 if (!hit) {
                     result.add(defaultValue);
                 }
             }
         }
         return result;
+    }
+
+    public Collection<String> skipTagsAndSort(Collection<String> input, String skipContent) {
+        java.util.TreeMap<String, String> output = new TreeMap<String, String>();
+        for (String item : input) {
+            output.put(
+                item.replaceAll("<<[^>]*>> *", ""),
+                item.replaceAll("<<|>>", "")
+            );
+        }
+        return skipContent.equals("true") ? (Collection<String>) (Collection<?>) output.keySet() : output.values();
     }
 }
 
